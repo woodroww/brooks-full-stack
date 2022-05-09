c992df3b7a0b504db31c202faffe09694c38a3d9
woodroww <example528@gmail.com>
2022-05-08 22:03:07 -0400

    ok moving along the crazyness with the toml file seems to be over need to see about nulls from db

--- backend/rust/todo_server/src/database/user_queries.rs
+++ backend/rust/todo_server/src/database/user_queries.rs
@@ -3,20 +3,30 @@
 
 //use tokio_postgres::{NoTls, Error, SimpleQueryMessage};
 use crate::database::UserId;
-use crate::routes::users::UserCreatedInfo;
 use crate::routes::errors::TodoAppError;
+use crate::routes::users::{UserCreatedInfo, User};
 
+use chrono::NaiveDateTime;
 use deadpool_postgres::Pool;
 
 pub async fn db_create_user(
     username: &str,
     password: &str,
     token: &str,
-    db_pool: &Pool) -> Result<UserCreatedInfo, TodoAppError> {
-
+    db_pool: &Pool,
+) -> Result<UserCreatedInfo, TodoAppError> {
     let con = db_pool.get().await.unwrap();
     let sql = "INSERT INTO users (username, password, token) VALUES ($1, $2, $3)";
-    let err = con.execute(sql, &[&username.to_string(), &password.to_string(), &token.to_string()]).await;
+    let err = con
+        .execute(
+            sql,
+            &[
+                &username.to_string(),
+                &password.to_string(),
+                &token.to_string(),
+            ],
+        )
+        .await;
 
     if err.is_ok() {
         let result = UserCreatedInfo {
@@ -26,7 +36,9 @@ pub async fn db_create_user(
         };
         Ok(result)
     } else {
-        Err(TodoAppError { name: "error from executing sql" })
+        Err(TodoAppError {
+            name: "error from executing sql",
+        })
     }
 }
 
@@ -34,8 +46,30 @@ pub fn db_hash_password(password: &str) {
     todo!()
 }
 
-pub fn db_get_by_username(username: &str) {
-    todo!()
+pub async fn db_get_by_username(
+    username: &str,
+    db_pool: &Pool
+) -> Result<User, TodoAppError> {
+    let con = db_pool.get().await.unwrap();
+    let sql = "SELECT * FROM users WHERE username = $1 LIMIT 1";
+    let result = con.query(sql, &[&username.to_string()]).await;
+    if let Ok(r) = result {
+        let mut result = User::default();
+        if let Some(user_row) = r.first() {
+            //let date: NaiveDateTime = user_row.get("deleted_at");
+            //println!("date string: {}", date);
+            result.id = user_row.get("id");
+            result.username = user_row.get("username");
+            result.password = user_row.get("password");
+            result.deleted_at = Some(user_row.get("deleted_at"));
+            result.token = user_row.get("token");
+        }
+        Ok(result)
+    } else {
+        Err(TodoAppError {
+            name: "invalid username or password",
+        })
+    }
 }
 
 pub fn db_find_and_remove_token(token: &str) {
--- backend/rust/todo_server/src/routes/users.rs
+++ backend/rust/todo_server/src/routes/users.rs
@@ -1,18 +1,24 @@
-use actix_web::{web, Error, HttpResponse, HttpRequest};
-use serde::{Deserialize, Serialize};
-use crate::routes::errors::TodoAppError;
-use chrono::{DateTime, Utc};
 use crate::database::UserId;
+use crate::routes::errors::TodoAppError;
+use actix_web::{web, Error, HttpRequest, HttpResponse};
+use chrono::NaiveDateTime;
+use serde::{Deserialize, Serialize};
 
-// file for sql table creation 
+// file for sql table creation
 // ../../../../../database/init.sql
 #[derive(Serialize, Deserialize)]
-struct User {
-    id: UserId,
-    username: String,
-    password: String,
-    deleted_at: Option<DateTime<Utc>>,
-    token: String,
+pub struct User {
+    pub id: UserId,
+    pub username: String,
+    pub password: String,
+    pub deleted_at: Option<NaiveDateTime>,
+    pub token: String,
+}
+
+impl Default for User {
+    fn default() -> Self {
+        Self { id: Default::default(), username: Default::default(), password: Default::default(), deleted_at: Default::default(), token: Default::default() }
+    }
 }
 
 // info returned from db_create_user
@@ -65,14 +71,14 @@ localhost:3010/api/v1/users \
 
 // Create user, create token from username, insert default tasks
 // return new user
-use crate::database::user_queries::db_create_user;
+use crate::database::user_queries::*;
 
 use deadpool_postgres::Pool;
 
 pub async fn create_user(
     body: web::Json<LoginInfo>,
-    pool: web::Data<Pool>) -> Result<HttpResponse, Error> {
-
+    pool: web::Data<Pool>,
+) -> Result<HttpResponse, Error> {
     let new_token = "createuserToken".to_string();
     let new_user = db_create_user(&body.username, &body.password, &new_token, &pool).await?;
     let response = CreateUserResponse { data: new_user };
@@ -81,7 +87,7 @@ pub async fn create_user(
 
 /*
 # login
-## route: "/login" 
+## route: "/login"
 
 curl -X POST \
 localhost:3010/api/v1/users/login \
@@ -91,28 +97,25 @@ localhost:3010/api/v1/users/login \
 ### response:
 (with different token than from the creation request)
 {
-	"data": {
-		"id": 3,
-		"username": "woodroww",
-		"token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Indvb2Ryb3d3IiwiaWF0IjoxNjUxODc4Mjg1fQ.KMLTPRSfhiKxfeVx4t1bF9VSUb7HsFOAZwwFcrtYLXE"
-	}
+    "data": {
+        "id": 3,
+        "username": "woodroww",
+        "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Indvb2Ryb3d3IiwiaWF0IjoxNjUxODc4Mjg1fQ.KMLTPRSfhiKxfeVx4t1bF9VSUb7HsFOAZwwFcrtYLXE"
+    }
 }
 */
 // get user from db, compare passwords, create login token
 // return user or error 500
 
-pub async fn login(body: web::Json<LoginInfo>) -> Result<HttpResponse, Error> {
-    let new_id = 1;
-    let new_token = "loginsjHdsewROUirwe".to_string();
-    let new_user = User {
-        id: new_id,
-        username: body.username.clone(),
-        password: "pass".to_string(),
-        deleted_at: None,
-        token: new_token,
-    };
-    let response = LoginResponse { data: new_user };
-    Ok(HttpResponse::Ok().json(response))
+pub async fn login(
+    body: web::Json<LoginInfo>,
+    pool: web::Data<Pool>,
+) -> Result<HttpResponse, TodoAppError> {
+    let result = db_get_by_username(&body.username, &pool).await;
+    match result {
+        Ok(user) => Ok(HttpResponse::Ok().json(LoginResponse { data: user })),
+        Err(error) => Err(error)
+    }
 }
 
 /*
@@ -136,25 +139,17 @@ pub async fn logout(req: HttpRequest) -> Result<HttpResponse, TodoAppError> {
         if let Some(token_string) = t.to_str().ok() {
             println!("we have token: {}", token_string);
         } else {
-            return Err(TodoAppError { name: "no x-auth-token string error" });
+            return Err(TodoAppError {
+                name: "no x-auth-token string error",
+            });
         }
     } else {
-        return Err(TodoAppError { name: "no x-auth-token error" });
+        return Err(TodoAppError {
+            name: "no x-auth-token error",
+        });
     }
-    let response = MessageResponse { message: "user logged out".to_string() };
+    let response = MessageResponse {
+        message: "user logged out".to_string(),
+    };
     Ok(HttpResponse::Ok().json(response))
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
